080b39b91d416caf4ed6bca34227c53b67a2c0be
P-once <60417356+P-once@users.noreply.github.com>
2026-01-31 23:33:53 -0800

    Updated boss ship sprites

--- Scripts/Bosses/PirateShip/pirate_ship.gd
+++ Scripts/Bosses/PirateShip/pirate_ship.gd
@@ -5,6 +5,7 @@ class_name Boss
 signal healthChanged
 
 @onready var cannons: Node2D = $Cannons
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const STARTING_HEALTH = 10000
 
@@ -12,6 +13,7 @@ var currentHealth = STARTING_HEALTH
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	animated_sprite_2d.play("opening")
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,3 +35,14 @@ func _on_hitable_area_area_entered(area: Area2D) -> void:
 		var bullet_damage = 20
 		take_damage(bullet_damage)
 		area.queue_free()
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	print("Finished playing " + animated_sprite_2d.animation)
+	if animated_sprite_2d.animation == "closing":
+		animated_sprite_2d.play("closed")
+	elif animated_sprite_2d.animation == "opening":
+		cannons.visible = true
+		var cannonArray: Array = cannons.get_children()
+		for cannon in cannonArray:
+			cannon.prepareFire()
+		animated_sprite_2d.play("opened")
